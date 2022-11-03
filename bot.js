bfc9b69cf330d7f9eb93dae4113c9bffbe96af80
fire7ly <fire7ly@gmail.com>
2022-11-03 15:43:51 +0530

    Add !alive command
    Signed-off-by: fire7ly <fire7ly@gmail.com>

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
+// import system modules
+const package = require('./package.json');
+
+
 // import 3rd party modules from npm 
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 
-
+// Define runtime veriables
+const OWNER = package.author || "fire7ly"
 
 const client = new Client();
 
@@ -14,6 +19,22 @@ client.on('qr', qr => {
 
 client.on('ready', () => {
     console.log('Client is ready!');
+});
+
+client.on('message', async message => {
+    let chat = await message.getChat();
+    const content = message.body
+
+    // set alive command response
+    if (content == '!alive') {
+        message.reply(`
+Hi I am ${ OWNER }\`s Whatsapp Bot 🤖
+I can do some enterainment untill 
+my owner get back online! You can 
+check my abilites by *!help* command
+        `);
+        console.log(`${ chat.name }`, 'request for', `${ content }`);
+    }
 });
 
 client.initialize();
